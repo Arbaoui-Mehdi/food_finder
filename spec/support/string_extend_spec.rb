c6f8b4cd5a1baa4a679120c883a70e4202c93166
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2017-06-15 23:25:44 -0700

    String Tests

--- spec/support/string_extend_spec.rb
+++ spec/support/string_extend_spec.rb
@@ -1,32 +1,53 @@
 describe 'String' do
 
-  describe "#titleize" do
+  describe '#titleize' do
 
-    it "capitalizes each word in a string"
+    it 'capitalizes each word in a string' do
+      string = String.new('hello World')
+      expect(string.titleize).to eq('Hello World')
+    end
     
-    it "works with single-word strings"
+    it 'works with single-word strings' do
+      expect('a'.titleize).to eq('A')
+    end
     
-    it "capitalizes all uppercase strings"
+    it 'capitalizes all uppercase strings' do
+      expect('HELLO'.titleize).to eq('Hello')
+    end
     
-    it "capitalizes mixed-case strings"
+    it 'capitalizes mixed-case strings' do
+      expect('HeLlo WOrld'.titleize).to eq('Hello World')
+    end
     
   end
-  
+
   describe '#blank?' do
 
-    it "returns true if string is empty"
+    it 'returns true if string is empty' do
+      expect(''.blank?).to be true
+    end
 
-    it "returns true if string contains only spaces"
+    it 'returns true if string contains only spaces' do
+      expect('   '.blank?).to be true
+    end
 
-    it "returns true if string contains only tabs"
-    # Get a tab using a double-quoted string with \t
-    # example: "\t\t\t"
+    it 'returns true if string contains only tabs' do
+      # Get a tab using a double-quoted string with \t
+      # example: '\t\t\t'
+      expect("\t\t\t".blank?).to be true
+    end
 
-    it "returns true if string contains only spaces and tabs"
+    it 'returns true if string contains only spaces and tabs' do
+      expect("  \t  \t".blank?).to be true
+    end
     
-    it "returns false if string contains a letter"
+    it 'returns false if string contains a letter' do
+      expect('h'.blank?).to be false
+    end
 
-    it "returns false if string contains a number"
+    it 'returns false if string contains a number' do
+      expect('1'.blank?).to be false
+    end
     
   end
   
